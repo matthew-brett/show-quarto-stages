07ec7a138bad8d5d7cc42b82a7eff236c58bcd7d
Matthew Brett <matthew.brett@gmail.com>
2024-11-11 12:35:57 +0000

    Clean up filter a little

--- add_callout_target.py
+++ add_callout_target.py
@@ -7,14 +7,14 @@ REPLACEMENT_NOTE = '''\
 ## My filtered heading
 :::'''
 
+
 def action(elem, doc):
     if (isinstance(elem, pf.Div) and
         'callout-note' in elem.classes and
         'filtered' in pf.stringify(elem)):
-        return pf.convert_text('''\
-::: {#nte-my-filtered-note .callout-note}
-## My filtered heading
-:::''', input_format='markdown', output_format='panflute')
+        return pf.convert_text(REPLACEMENT_NOTE,
+                               input_format='markdown',
+                               output_format='panflute')
 
 
 def main(doc=None):
